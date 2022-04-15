e424346f4c99bc153751ca8a9ebb90e47f157f13
Rowan Holop (They/Them) <57956151+rholop@users.noreply.github.com>
2022-04-15 17:47:29 -0400

    Update request.service.ts

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -23,7 +23,7 @@ export class RequestService {
   private httpOptions = {
     headers: new HttpHeaders({
       Authorization:
-        'Bearer tfp_4jx9QAhwEkX84j4kKKwUnKHWE4QD4J4sCeLbf1ej6o1g_3pYNDaENnhuQBv',
+        'Bearer <personal access token>',
     }),
   };
 
